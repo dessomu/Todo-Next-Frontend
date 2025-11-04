947a23587ad262ca2c3c001ea28d85eff80c4b78
Somnath Baidya <somnathbaidya@Somnaths-MacBook-Air.local>
2025-11-04 12:25:31 +0530

    renamed proxy to middleware

--- middleware.js
+++ middleware.js
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 
 export function middleware(req) {
   const token = req.cookies.get("auth_token")?.value;
-  console.log(token);
 
   if (!token && req.nextUrl.pathname !== "/login") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   // Has token but trying to go to login → send to home
-  if (token && req.nextUrl.pathname !== "/home") {
+  if (token && req.nextUrl.pathname === "/login") {
     console.log("✅ Token found → redirecting to /home");
     return NextResponse.redirect(new URL("/home", req.url));
   }
